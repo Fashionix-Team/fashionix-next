62f42243ad9a35c8dadbaa756773f542d0840be2
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-11-18 23:53:18 +0700

    Revert "Membuat halaman Detail produk & Menyambungkannya ke backend 80%"
    This reverts commit c664348f276cf1c6680582e72a9f4aaa60a78261.

--- app/(public)/product/detail/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import DetailProduk from "@/components/product/detail-produk";
-
-export { generateMetadata } from "@/components/product/detail-produk";
-
-export const revalidate = 60;
-export const dynamic = "force-dynamic";
-
-export default DetailProduk;
